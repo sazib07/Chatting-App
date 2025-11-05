82d72999b00372f4ce591ca01ef05c687db16588
sazib07 <sazibmd93@gmail.com>
2025-11-05 23:35:59 +0600

    line up

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -74,54 +74,7 @@ setFriendList(array);
   });
     },[]);
   return (
-    <div className="flex h-[90vh] bg-gray-100 rounded-xl shadow-lg overflow-hidden">
-      {/* Sidebar */}
-      <div className="w-1/4 bg-white border-r border-gray-200 p-4 flex flex-col">
-        <h2 className="text-2xl font-bold mb-4 text-gray-700">Friends</h2>
-        <ul className="space-y-2 overflow-y-auto flex-1">
-          {friendList.map((item) => (
-
-            <li
-              key={item.id}
-              onClick={() => handleSelectFriend(item)}
-              className={`flex items-center p-3 rounded-lg cursor-pointer transition 
-                ${
-                  selecteduser.id == item.senderid || selecteduser.id == item.reciverid
-                    ? "bg-blue-300 text-black"
-                    : " bg-gray-500"
-                }`}
-            >
-              <div className="relative w-10 h-10">
-                <img
-                  src={`https://api.dicebear.com/9.x/initials/svg?seed=${item.sendername}`}
-                  alt={item.name}
-                  className="w-10 h-10 rounded-full"
-                />
-                <span
-                  className={`absolute bottom-0 right-0 w-3 h-3 rounded-full border-2 border-white ${
-                   item.status === "online"
-                      ? "bg-green-500"
-                      : "bg-gray-400"
-                  }`}
-                ></span>
-                  
-           
-              </div>
-              {user.uid == item.senderid ?
-              <p className="ml-3 font-medium">{item.recivername}</p>
-              :
-              <p className="ml-3 font-medium">{item.sendername}</p>
-            }
-            {/* <div>
-            <p className="ml-3 font-medium">{item.senderemail}</p>
-            
-            </div> */}
-          
-            </li>
-          ))}
-          <button className="bg-blue-500 text-white px-2 py-1">Block</button>
-        </ul>
-      </div>
+    
 
       {/* Chat Area */}
       <div className="flex-1 flex flex-col">
