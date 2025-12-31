824d46ffe30bbdc47c5ca143984d6d3b6456e776
sazib07 <sazibmd93@gmail.com>
2026-01-01 03:11:42 +0600

    bug fixed

--- src/Store.js
+++ src/Store.js
@@ -2,7 +2,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 // Import slices (ensure filenames match exactly!)
-import { userSlice } from './Slice/userslice'      // file: src/Slice/userSlice.js
+import userSlice from './Slice/userslice'      // file: src/Slice/userSlice.js
 import messageSlice from './Slice/messageSlice' // file: src/Slice/messageSlice.js
 
 export const store = configureStore({
